fbab0e1978a412c3cde04002c592f321dd58a0b3
Zethson <lukas.heumos@gmail.com>
2017-05-21 15:30:11 +0200

    refactoring

--- src/model/Tree.java
+++ src/model/Tree.java
@@ -12,9 +12,9 @@ import java.util.Map;
  */
 public class Tree {
     private TaxonNode root;
-    private HashMap<Integer, TaxonNode> treeStructure; //TODO: Not sure if name is appropriate, maybe refactor it
+    private HashMap<Integer, TaxonNode> treeStructure;
 
-    public Tree(TaxonNode root){
+    public Tree(TaxonNode root) {
         this.treeStructure = treeStructure;
         treeStructure.put(root.getTaxonId(), root);
     }
@@ -22,63 +22,46 @@ public class Tree {
     //TODO
 
     /**
-     *
      * @param node
      */
-    public void addNode(TaxonNode node){
+    public void addNode(TaxonNode node) {
+        if (root == null) {
+            root = node;
+        } else {
+            treeStructure.put(node.getTaxonId(), node);
 
-     if ( root == null ) {
+            //Searches the tree for every child of the new added node and adds the found child to the list of child notes.
+            //It does also register the new node object as parent of the found child nodes
+            for (Map.Entry<Integer, TaxonNode> entry : treeStructure.entrySet()) {
+                if (entry.getValue().getParentId() == node.getTaxonId()) {
+                    TaxonNode foundChild = entry.getValue();
+                    foundChild.setParentNode(node); // Maybe not necessary
+                    node.getChildNodeList().add(foundChild);
+                }
 
-         root = node;
-
-     } else {
-
-        treeStructure.put(node.getTaxonId(), node);
-
-        //Searches the tree for every child of the new added node and adds the found child to the list of child notes.
-        //It does also register the new node object as parent of the found child nodes
-
-        for (Map.Entry<Integer, TaxonNode> entry : treeStructure.entrySet()) {
-
-            if ( entry.getValue().getParentId() == node.getTaxonId() ) {
-
-                TaxonNode foundChild = entry.getValue();
-
-                foundChild.setParentNode(node); // Maybe not necessary
-
-                node.getChildNodeList().add(foundChild);
-
+                //If the tree already contains the father of the new node, register this parent node object as parent node of the new node
+                //and register the new node object as child of the found parent
+                if (entry.getValue().getTaxonId() == node.getParentId()) {
+                    TaxonNode foundParent = entry.getValue();
+                    node.setParentNode(foundParent);
+                    foundParent.getChildNodeList().add(node);
+                }
             }
 
-            //If the tree already contains the father of the new node, register this parent node object as parent node of the new node
-            //and register the new node object as child of the found parent
-            if ( entry.getValue().getTaxonId() == node.getParentId()) {
-
-                TaxonNode foundParent = entry.getValue();
-
-                node.setParentNode(foundParent);
-
-                foundParent.getChildNodeList().add(node);
-            }
         }
-
-     }
     }
 
 
     /**
      * Builds the tree structure based on the parsed information from the NamesDmp and NodesDmp files
+     *
      * @param listOfNodesDmp - ArrayList of NodesDmp class.
      * @param listOfNamesDmp - ArrayList of NamesDmp class.
      */
     public void buildTree(ArrayList<NodesDmp> listOfNodesDmp, ArrayList<NamesDmp> listOfNamesDmp) {
-
-        for ( int i = 0; i < listOfNodesDmp.size(); i++ ) {
-
-            for ( int j = 0; j < listOfNamesDmp.size(); j++ ) {
-
-                if ( listOfNodesDmp.get(i).getId() == listOfNamesDmp.get(j).getId() ) {
-
+        for (int i = 0; i < listOfNodesDmp.size(); i++) {
+            for (int j = 0; j < listOfNamesDmp.size(); j++) {
+                if (listOfNodesDmp.get(i).getId() == listOfNamesDmp.get(j).getId()) {
                     //if there's no root yet
                     addNode(new TaxonNode(listOfNamesDmp.get(j).getName(), listOfNamesDmp.get(i).getId(), listOfNodesDmp.get(i).getRank(),
                             /*null try out new way*/ listOfNodesDmp.get(i).getParentId(), new ArrayList<TaxonNode>()));
@@ -87,7 +70,6 @@ public class Tree {
             }
 
         }
-        //tree.setRoot(new TaxonNode());
     }
 
 
@@ -97,7 +79,9 @@ public class Tree {
         return root;
     }
 
-    public HashMap<Integer, TaxonNode> getTreeStructure() { return treeStructure; }
+    public HashMap<Integer, TaxonNode> getTreeStructure() {
+        return treeStructure;
+    }
 
 
     //SETTER
