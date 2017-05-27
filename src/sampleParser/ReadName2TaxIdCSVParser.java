094d0df53170f3c32078c4acb30f29598f6ea0c7
Zethson <lukas.heumos@gmail.com>
2017-05-27 09:49:36 +0200

    setup test folder

--- src/sampleParser/ReadName2TaxIdCSVParser.java
+++ src/sampleParser/ReadName2TaxIdCSVParser.java
@@ -13,17 +13,17 @@ import java.util.Iterator;
 /**
  * Created by julian on 15.05.17.
  */
-public class ReadName2TaxIdCSVParser implements InputFile{
+public class ReadName2TaxIdCSVParser implements InputFile {
     @Override
-    public ArrayList<Sample> parse(String filepath) throws IOException{
+    public ArrayList<Sample> parse(String filepath) throws IOException {
         /*TODO: I need to access the tree here I guess, but don't know yet how that is done
           Right now, an empty HashMap "treeStructure" is used
         */
         HashMap<Integer, TaxonNode> treeStructure = new HashMap<>(); //This should be a hashmap that maps taxon ids to TaxonNode objects
         HashMap<TaxonNode, Integer> node2CountMap = new HashMap<>(); //This one maps nodes to their read counts
         //Initialize all read counts with zero
-        for(TaxonNode node: treeStructure.values()){
-            node2CountMap.put(node,0);
+        for (TaxonNode node : treeStructure.values()) {
+            node2CountMap.put(node, 0);
         }
 
         //Initialize the object to be returned
@@ -35,19 +35,19 @@ public class ReadName2TaxIdCSVParser implements InputFile{
         String line = reader.readLine();
         String[] lineSplit;
         int currentTaxonId;
-        while(line!=null){
+        while (line != null) {
             lineSplit = line.split(",");
             currentTaxonId = Integer.parseInt(lineSplit[1]);
             //Get the TaxonNode for this sample
             TaxonNode currentTaxonNode = treeStructure.get(currentTaxonId);
             //Check if this taxon is already in the sample
-            HashMap<TaxonNode,Integer> taxa2CountMap = sample.getTaxa2CountMap();
-            if(taxa2CountMap.containsKey(currentTaxonNode)){
+            HashMap<TaxonNode, Integer> taxa2CountMap = sample.getTaxa2CountMap();
+            if (taxa2CountMap.containsKey(currentTaxonNode)) {
                 //Increment the count of this taxon in the sample by one
-                taxa2CountMap.put(currentTaxonNode,taxa2CountMap.get(currentTaxonNode)+1);
-            }else{
+                taxa2CountMap.put(currentTaxonNode, taxa2CountMap.get(currentTaxonNode) + 1);
+            } else {
                 //Else add the taxon to the sample and set its count to one
-                taxa2CountMap.put(currentTaxonNode,1);
+                taxa2CountMap.put(currentTaxonNode, 1);
             }
 
             line = reader.readLine();
---
+++ test/sampleParser/ReadName2TaxIdCSVParserTest.java
@@ -0,0 +1,23 @@
+package sampleParser;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Zeth on 27.05.2017.
+ */
+public class ReadName2TaxIdCSVParserTest {
+
+    @Before
+    public void setUp() throws Exception {
+        ReadName2TaxIdCSVParser readName2TaxIdCSVParser;
+    }
+
+    @Test
+    public void parse() throws Exception {
+
+    }
+
+}
