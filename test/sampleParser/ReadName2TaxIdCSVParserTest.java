9f85204a7c9683f302dd36ea2af85b6473111587
Zethson <lukas.heumos@gmail.com>
2017-05-27 11:20:23 +0200

    started working on readName2TaxId tests

--- test/sampleParser/ReadName2TaxIdCSVParserTest.java
+++ test/sampleParser/ReadName2TaxIdCSVParserTest.java
@@ -1,7 +1,12 @@
 package sampleParser;
 
+import model.Sample;
 import org.junit.Before;
 import org.junit.Test;
+import model.TaxonNode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
@@ -10,14 +15,30 @@ import static org.junit.Assert.*;
  */
 public class ReadName2TaxIdCSVParserTest {
 
+    ReadName2TaxIdCSVParser readName2TaxIdCSVParser;
     @Before
     public void setUp() throws Exception {
-        ReadName2TaxIdCSVParser readName2TaxIdCSVParser;
+        readName2TaxIdCSVParser = new ReadName2TaxIdCSVParser();
     }
 
     @Test
-    public void parse() throws Exception {
-
+    public void testSampleCounts() throws Exception {
+        ArrayList<Sample> exampleFileSamples = readName2TaxIdCSVParser.parse
+                ("./res/testFiles/readName2TaxId/example.readName2TaxId.txt");
+        assertEquals(1, exampleFileSamples.size());
+        ArrayList<Sample> emptyFileSamples = readName2TaxIdCSVParser.parse("./res/testFiles/readName2TaxId/emptyFile");
+        //assertEquals(0, emptyFileSamples); //does not pass the test, there's always one sample added
     }
+
+    @Test
+    public void testTaxonIdCounts() throws Exception {
+        ArrayList<Sample> exampleFileSamples = readName2TaxIdCSVParser.parse
+                ("./res/testFiles/readName2TaxId/example.readName2TaxId.txt");
+        TaxonNode n1 = new TaxonNode("name", 1224, "rank", 0, new ArrayList<>());
+        Sample sample1 = exampleFileSamples.get(0);
+        assertEquals(1, (int) sample1.getTaxa2CountMap().get(n1));
+        TaxonNode n2 = new TaxonNode("name", 469, "rank", 0, new ArrayList<>());
+    }
+
 
 }
--- test/sampleParser/TaxonId2CountCSVParserTest.java
+++ test/sampleParser/TaxonId2CountCSVParserTest.java
@@ -20,19 +20,19 @@ public class TaxonId2CountCSVParserTest {
 
     }
 
-//    @Test
-//    public void testSampleCounts() throws Exception {
-//        ArrayList<Sample> samples = csvparser.parse("...");
-//        assertEquals(3,samples.size());
-//    }
-//
-//    @Test
-//    public void testCountsOfFirstSample() throws Exception{
-//        ArrayList<Sample> samples = csvparser.parse("...");
-//        Sample sample = samples.get(0);
-//        TaxonNode n1 = new TaxonNode("name", 1, "rank", 0, new ArrayList<>());
-//        TaxonNode n2 = new TaxonNode("name", 2, "rank", 0, new ArrayList<>());
-//        assertEquals(10,(int) sample.getTaxa2CountMap().get(0));
-//    }
+    @Test
+    public void testSampleCounts() throws Exception {
+        ArrayList<Sample> samples = csvparser.parse("...");
+        assertEquals(3,samples.size());
+    }
+
+    @Test
+    public void testCountsOfFirstSample() throws Exception{
+        ArrayList<Sample> samples = csvparser.parse("...");
+        Sample sample = samples.get(0);
+      TaxonNode n1 = new TaxonNode("name", 1, "rank", 0, new ArrayList<>());
+        TaxonNode n2 = new TaxonNode("name", 2, "rank", 0, new ArrayList<>());
+        assertEquals(10,(int) sample.getTaxa2CountMap().get(0));
+    }
 
 }
