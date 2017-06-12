ec84cdd3cf797ee9fcc4167d0cfc8cde415187e9
jmueller95 <julian.mueller@student.uni-tuebingen.de>
2017-06-12 14:32:38 +0200

    continued implementing SampleComparison

--- src/analysis/SampleComparison.java
+++ src/analysis/SampleComparison.java
@@ -2,6 +2,12 @@ package analysis;
 
 import model.Sample;
 import model.TaxonNode;
+import org.apache.commons.math3.stat.correlation.KendallsCorrelation;
+import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
+import org.apache.commons.math3.stat.correlation.SpearmansCorrelation;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Class for the comparison of TWO Sample objects.
@@ -18,7 +24,8 @@ public class SampleComparison {
 
     /**
      * Subtracts the count of a given taxon in the secondSample from the corresponding count in the firstSample
-     * Returns -1 if taxonNode can't be found in both of the samples
+     * Returns -1 if taxonNode can't be found in both of the samples (TODO: Maybe avoid this by intializing every node with count = 0)
+     * TODO: Decide whether only count for this taxon or recursive count is used
      */
     public int taxonCountDifference(TaxonNode taxonNode) {
         if (firstSample.getTaxa2CountMap().containsKey(taxonNode) && secondSample.getTaxa2CountMap().containsKey(taxonNode)) {
@@ -37,4 +44,35 @@ public class SampleComparison {
 
 
 
+    /**
+     * Accepts a list of taxonNodes and a string that specifies which kind of correlation should be calculated (either
+     * "pearson", "spearman", or "kendall").
+     * Calculates the counts of these nodes in both of the samples and returns the pearson/spearman/kendall
+     * correlation coefficient of these two double arrays.
+     * TODO: Either handle nodes that don't appear in both samples or intialize EVERY node with zero when parsing
+     *
+     * @param nodesList
+     * @param correlation_type
+     * @return correlation_coefficient
+     */
+    public double calculateCorrelation(List<TaxonNode> nodesList, String correlation_type) {
+        double[] countsInFirstSample = new double[nodesList.size()];
+        double[] countsInSecondSample = new double[nodesList.size()];
+        for (int i = 0; i < nodesList.size(); i++) {
+            countsInFirstSample[i] = firstSample.getTaxonCountRecursive(nodesList.get(i));
+            countsInSecondSample[i] = secondSample.getTaxonCountRecursive(nodesList.get(i));
+        }
+        double correlation_coefficient = 0;
+        switch (correlation_type) {
+            case "pearson":
+                correlation_coefficient = new PearsonsCorrelation().correlation(countsInFirstSample, countsInSecondSample);
+                break;
+            case "spearman":
+                correlation_coefficient = new SpearmansCorrelation().correlation(countsInFirstSample, countsInSecondSample);
+                break;
+            case "kendall":
+                correlation_coefficient = new KendallsCorrelation().correlation(countsInFirstSample, countsInSecondSample);
+        }
+        return correlation_coefficient;
+    }
 }
--- test/analysis/SampleComparisonTest.java
+++ test/analysis/SampleComparisonTest.java
@@ -5,6 +5,8 @@ import model.TaxonNode;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.*;
+
 import static org.junit.Assert.*;
 
 /**
@@ -12,11 +14,17 @@ import static org.junit.Assert.*;
  */
 public class SampleComparisonTest {
     private TaxonNode node1, node2, node3, node4;
-    private Sample sample1, sample2;
-    private SampleComparison sampleComparison;
+
+    @Before
+    public void setUp() throws Exception {
+        node1 = new TaxonNode(1, null, 0);
+        node2 = new TaxonNode(2, null, 0);
+        node3 = new TaxonNode(3, null, 0);
+        node4 = new TaxonNode(4, null, 0);
+    }
 
     /**
-     * Creates 4 taxon nodes, adds 3 to each sample such that each sample has a node the other one doesn't have
+     * Adds 3 of the four nodes to each sample such that each sample has a node the other one doesn't have
      * Then creates the SampleComparison object with the two samples
      * Afterwards the samples look like this:
      * id ||  sample1 |   sample2
@@ -27,29 +35,41 @@ public class SampleComparisonTest {
      *
      * @throws Exception
      */
-    @Before
-    public void setUp() throws Exception {
-        node1 = new TaxonNode(1, null, 0);
-        node2 = new TaxonNode(2, null, 0);
-        node3 = new TaxonNode(3, null, 0);
-        node4 = new TaxonNode(4, null, 0);
+    @Test
+    public void testCountDiff() throws Exception {
 
-        sample1 = new Sample();
+        Sample sample1 = new Sample();
         sample1.getTaxa2CountMap().put(node1, 10);
         sample1.getTaxa2CountMap().put(node2, 52);
         sample1.getTaxa2CountMap().put(node3, 1);
-        sample2 = new Sample();
+        Sample sample2 = new Sample();
         sample2.getTaxa2CountMap().put(node2, 10);
         sample2.getTaxa2CountMap().put(node3, 10);
         sample2.getTaxa2CountMap().put(node4, 4);
-        sampleComparison = new SampleComparison(sample1, sample2);
-    }
-
-    @Test
-    public void testCountDiff() throws Exception {
+        SampleComparison sampleComparison = new SampleComparison(sample1, sample2);
         assertEquals(42, sampleComparison.taxonCountDifference(node2));
         assertEquals(-9, sampleComparison.taxonCountDifference(node3));
         assertEquals(-1, sampleComparison.taxonCountDifference(node1));
     }
 
+    @Test
+    public void testCalculateCorrelation() throws Exception {
+        Sample sample1 = new Sample();
+        sample1.getTaxa2CountMap().put(node1,10);
+        sample1.getTaxa2CountMap().put(node2,20);
+        sample1.getTaxa2CountMap().put(node3,10);
+        sample1.getTaxa2CountMap().put(node4,11);
+
+        Sample sample2 = new Sample();
+        sample2.getTaxa2CountMap().put(node1,10);
+        sample2.getTaxa2CountMap().put(node2,20);
+        sample2.getTaxa2CountMap().put(node3,10);
+        sample2.getTaxa2CountMap().put(node4,10);
+
+        SampleComparison sampleComparison = new SampleComparison(sample1,sample2);
+        List<TaxonNode> nodesList1 = Arrays.asList(node1,node2);
+        System.out.println(sampleComparison.calculateCorrelation(nodesList1, "pearson"));
+        List<TaxonNode> nodesList2 = Arrays.asList(node3, node4);
+        System.out.println(sampleComparison.calculateCorrelation(nodesList2, "pearson"));
+    }
 }
