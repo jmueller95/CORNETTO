b5022f6c2c12705a2d2dd52c2741d439e9c5258b
NantiaL <nadia_leonidou@yahoo.gr>
2017-06-13 18:59:04 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	test/sampleParser/TaxonId2CountCSVTest.java

--- test/util/DownloadFilesHelperTest/DownloadNodesAndNameDMPFilesTest.java
+++ test/util/DownloadFilesHelperTest/DownloadNodesAndNameDMPFilesTest.java
@@ -1,40 +1,40 @@
 package util.DownloadFilesHelperTest;
 
-import model.Sample;
 import org.junit.Before;
-
-import static org.junit.Assert.*;
-import org.apache.commons.io.FileUtils;
 import org.junit.Test;
-import sampleParser.TaxonId2CountCSVParser;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.net.URL;
+import util.DownloadFilesHelper.DownloadNodesAndNameDMPFiles;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
+
+import static org.junit.Assert.assertEquals;
+
 
 /**
  * Created by NantiaL on 06.06.2017.
  */
 public class DownloadNodesAndNameDMPFilesTest {
+    DownloadNodesAndNameDMPFiles download;
+
     @Before
-    //public void setUp() throws Exception {
-
-
-   // }
-
-    @Test
-    public void testDownloadile() throws Exception {
-        String outputFolder = "./res";
-        private final String Url = " ./res/testFiles/example.taxonId2Count.txt";
-        URL zipUrl =  new URL(Url);
-        assertEquals("Done downloading file",  downloadFile(zipUrl, outputFolder) );  //in order to test if there are 29 organisms
+    public void setUp() throws Exception {
+        download = new DownloadNodesAndNameDMPFiles();
 
     }
+
+    @Test
+    public void testDownloadAndUnpackFile() throws Exception {
+        download.DownloadNamesNodesDMPandUnzip();
+    }
+
+
+    @Test
+    public void testUnzipIt() throws Exception {
+        ArrayList<String> fileList = new ArrayList<>();
+        fileList.add("example.readName2TaxId.txt");
+        fileList.add("example.taxonId2Count.txt");
+        download.unZipIt("TwoZipFilesTest",  "./res/zipFileTest", fileList);
+    }
+
+
+
 
 }
