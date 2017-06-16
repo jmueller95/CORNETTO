109b17ffdac537c870dc24b18cdba41e49baab19
kaikias <samsamayn@yahoo.de>
2017-06-16 21:45:01 +0200

    Added a treeview gui element to the left pane. First idea how to display opened files: Opening a file now adds a new element to the treeview list and displays the file name and a placeholder ID as tree element on the left pane.

--- src/UI/Controller.java
+++ src/UI/Controller.java
@@ -1,16 +1,14 @@
 package UI;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 import model.TaxonTree;
 import treeParser.TreeParser;
 import util.DownloadFilesHelper.DownloadNodesAndNameDMPFiles;
-
-import javax.swing.plaf.FileChooserUI;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -23,9 +21,14 @@ public class Controller implements Initializable {
     private static final String NAMESDMPSRC = "./res/names.dmp";
     private static TaxonTree taxonTree;
 
+    private static int id = 1;
+
     //Elements of the GUI
     @FXML
     private Label leftLabel;
+
+    @FXML
+    private TreeView<String> treeViewFiles;
 
     /**
      * initializes all required files
@@ -36,12 +39,13 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         //build the tree
-        System.out.println("Downloading files...");
+        /*System.out.println("Downloading files...");
         setUpRequiredFiles();
         System.out.println("Parsing tree...");
         TreeParser treeParser = new TreeParser();
         treeParser.parseTree(NODESDMPSRC, NAMESDMPSRC);
-        taxonTree = treeParser.getTaxonTree();
+        taxonTree = treeParser.getTaxonTree();*/
+        initializeTreeView();
     }
 
     /**
@@ -59,22 +63,48 @@ public class Controller implements Initializable {
         }
     }
 
+    /**
+     * Initializes the tree view on left pane
+     */
+    public void initializeTreeView() {
+        treeViewFiles.setRoot(new TreeItem<>("root"));
+        treeViewFiles.setShowRoot(false);
+    }
+
     @FXML
     /**
-     * Opens a file chooser
+     * Opens a file chooser and gives the user the possibility to select a file
      */
     public void handleOpenFile() {
         File selectedFile = new FileChooser().showOpenDialog(null);
 
         boolean isFileFound = selectedFile != null;
-        leftLabel.setText(isFileFound ? selectedFile.getName() : "No such file found.");
+        //leftLabel.setText(isFileFound ? selectedFile.getName() : "No such file found.");
+        if (isFileFound) {
+            addFileToTreeView(selectedFile);
+        }
+    }
+
+    /**
+     * Adds and displays the file name and a placeholder id for the file name in the treeview gui element
+     * @param file
+     */
+    public void addFileToTreeView(File file) {
+
+        //TODO: Implement check for correct file extension
+        TreeItem<String> newEntity = new TreeItem<>(file.getName());
+
+        TreeItem<String> entityId = new TreeItem<>("Id: " + id++);
+
+        newEntity.getChildren().add(entityId);
+        treeViewFiles.getRoot().getChildren().add(newEntity);
     }
 
     /**
      * Exits the program
      */
     @FXML
-    public void handleExit(){
+    public void handleExit() {
         //TODO: Implement asking the user for saving the progress
         System.exit(0);
     }
