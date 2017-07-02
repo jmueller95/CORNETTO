7d0706a5243b98e13938c607fab22eae571f1da7
NantiaL <nadia_leonidou@yahoo.gr>
2017-07-02 23:17:02 +0200

    put a sample ID into the Sample class

--- src/model/Sample.java
+++ src/model/Sample.java
@@ -7,18 +7,28 @@ import java.util.HashMap;
  */
 public class Sample {
     private HashMap<TaxonNode, Integer> taxa2CountMap;
-    private HashMap<String, String> metaData;
+    public static HashMap<String, String> metaData;
+
+    public   static String SampleId ;
+    public   static String getSampleId()
+    {
+        return SampleId;
+    }
+
+
 
     // Empty constructor needed in CSV Parsers
     public Sample() {
         this.taxa2CountMap = new HashMap<>();
         this.metaData = new HashMap<>();
+        this.SampleId = new String();
     }
 
     // Constructor with initialisation data
     public Sample(HashMap<TaxonNode, Integer> taxa2CountMap, HashMap<String, String> metaData) {
         this.taxa2CountMap = taxa2CountMap;
         this.metaData = metaData;
+        this.SampleId = SampleId;
     }
 
     /**
