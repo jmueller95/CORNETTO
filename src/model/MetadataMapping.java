f33aa94de00c757c5500ba58e808e44d533f8f2e
NantiaL <nadia_leonidou@yahoo.gr>
2017-07-02 23:23:41 +0200

    1)created a mapping class for the metadata files , 2)created a class with sets of colours for our issue : 'Different Colour schemes .......'

--- src/model/MetadataMapping.java
+++ src/model/MetadataMapping.java
@@ -4,11 +4,8 @@ package model;
  * Created by NantiaL on 02.07.2017.
  */
 public class MetadataMapping {
-    package model;
 
-import com.sun.xml.internal.fastinfoset.util.StringArray;
 
-import java.util.HashMap;
 
     /**
      * Created by NantiaL on 30.06.2017.
