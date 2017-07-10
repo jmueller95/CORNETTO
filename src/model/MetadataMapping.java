286ac4d674105543a2a40fc2c8c23e0e94413ae3
NantiaL <nadia_leonidou@yahoo.gr>
2017-07-10 08:08:19 +0200

    * not finished*

--- src/model/MetadataMapping.java
+++ src/model/MetadataMapping.java
@@ -1,54 +1,55 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by NantiaL on 02.07.2017.
  */
 public class MetadataMapping {
 
 
-
-    /**
-     * Created by NantiaL on 30.06.2017.
-     */
-
-
-        Sample SampleID = new Sample();
-        private static String barcodeSequence ;
-        private static String header;
-
-        //Getters
-        public static String getBarcodeSequence() {
-            return barcodeSequence;
-        }
-
-        public static String getHeader() {
-            return header;
-        }
-
-        public Sample getSampleID() {
-            return SampleID;
-        }
-
-        //empty constructor
-        public MetadataMapping() {
-            this.barcodeSequence = new String();
-            this.header = new String();
-            this.SampleID = new Sample();
-        }
-
-        // Constructor with data
-        public MetadataMapping(Sample SampleID, String header,String barcodeSequence) {
-            this.SampleID = SampleID;
-            this.header = header;
-            this.barcodeSequence = barcodeSequence;
-        }
+    Sample SampleID = new Sample();
+    Sample MetadataMap = new Sample();
+    private ArrayList<Sample> sampleList;
 
 
+    //Getters
 
-
-
-
-
+    public Sample getMetadataMap() {
+        return MetadataMap;
     }
+
+    //empty constructor
+    public MetadataMapping() {
+        this.SampleID = new Sample();
+        this.MetadataMap = new Sample();
+    }
+
+    // Constructor with data
+    public MetadataMapping(Sample SampleID, String header, String barcodeSequence) {
+        this.SampleID = SampleID;
+        this.MetadataMap = MetadataMap;
+    }
+
+    public ArrayList<Sample> MetadataParser(String filepath) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filepath));
+        String line = reader.readLine();
+        String[] split = line.split(" ");
+        int id = split.length - 1; //shows that the first column is the sample ID
+
+
+      //  int numberOfData = split.length - 1;
+      //  HashMap<String, String> map = getMetadataMap().getMetaData();
+
+
+
+        return sampleList;
+    }
+
+}
 
 
