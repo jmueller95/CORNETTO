3b3a1e2b56f7ffb8079d4a8960db0d6d0160097c
NantiaL <nadia_leonidou@yahoo.gr>
2017-07-24 22:48:57 +0200

    color Gradient

--- src/view/MyColorGradient.java
+++ src/view/MyColorGradient.java
@@ -15,55 +15,63 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import static javafx.application.Application.getUserAgentStylesheet;
+import static javafx.application.Application.launch;
+
 
 /**
  * Created by NantiaL on 22.07.2017.
  */
-public class MyColorGradient //extends Application
- {
+public class MyColorGradient {
 
 //private   final static Map<Integer, Color> map = initNumberToColorMap();
 //private    static int factor;
-    //define colors
+/*define colors
     Color Red = Color.RED;
     Color Green = Color.GREEN;
     Color Pink = Color.PINK;
     Color Blue = Color.BLUE;
     MultipleGradientPaint.CycleMethod Reflect = MultipleGradientPaint.CycleMethod.REFLECT;
+*/
 
+    /**
+     * Return a map with all possible colors in JavaFX. Value contains an instance of a color object and the
+     * key is the static name
+     */
 
-     /**
-      * Return a Map with all all defined colors in JavaFX. The key is the static
-      * name of color and the value contains an instance of a Color object.
-      */
-     public Map<String, Color> getJavaFXColorMap() {
-         Field[] declaredFields = java.awt.Color.class.getDeclaredFields();
-         Map<String, Color> colors = new HashMap<>();
-         for (Field field : declaredFields) {
-             if (Modifier.isStatic(field.getModifiers()) && Modifier.isPublic(field.getModifiers())) {
-                 try {
-                     colors.put(field.getName(), (Color)field.get(null));
-                 } catch (NoSuchFieldError | SecurityException | IllegalArgumentException | IllegalAccessException ex) {
-                     Logger.getLogger(JavaFXColorChooser.class.getName()).log(Level.SEVERE, null, ex);
-                 }
-             }
-         }
-         return colors;
-     }
+    public Map<String, Color> getJavaFXColorMap() {
+        Field[] fields = java.awt.Color.class.getDeclaredFields();
+        Map<String, Color> color = new HashMap<>();
+        for (Field field : fields) {
+            if (Modifier.isStatic(field.getModifiers()) && Modifier.isPublic(field.getModifiers())) {
+                try {
+                    color.put(field.getName(), (Color) field.get(null));
+                } catch (NoSuchFieldError | SecurityException | IllegalArgumentException | IllegalAccessException ex) {
+                    Logger.getLogger(JavaFXColorChooser.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        return color;
+    }
+
+    public static void main(String[] args) {
+        // launch(args);
+    }
+
 /*
     @Override
-        //using Linear Gradient
-        public void start (Stage r){
+    //using Linear Gradient
+    public void start(Stage r) {
 
 
         //color stop-list for the gradient
         Stop Stop1 = new Stop(0, Red);
         Stop Stop2 = new Stop(1, Green);
-        Stop Stop3 = new Stop(2,Pink);
+        Stop Stop3 = new Stop(2, Pink);
         Stop[] end = new Stop[]{Stop1, Stop2, Stop3};
 
         //constructs the color Gradient
-        LinearGradient linearGradient = new LinearGradient(2, 0, 1, 0,true, Reflect, end);
+        LinearGradient linearGradient = new LinearGradient(2, 0, 1, 0, true, Reflect, end);
 
         //sets the frame
         VBox boxFrame = new VBox();
@@ -82,78 +90,22 @@ public class MyColorGradient //extends Application
         r.show();
     }
 
-        private final static int LOW = 0;
-        private final static int HIGH = 300;
-        private final static int HALF = (HIGH + 1) / 2;
+    private final static int LOW = 0;
+    private final static int HIGH = 300;
+    private final static int HALF = (HIGH + 1) / 2;
 
-        private    static int factor;
+    private static int factor;
 
-        private static void initList(final HashMap<Integer, Color> localMap) {
-            java.util.List<Integer> list = new ArrayList<Integer>(localMap.keySet());
-            Collections.sort(list);
-            Integer min = list.get(0);
-            Integer max = list.get(list.size() -1);
-            factor = max + 1;
-        }
-
-        /**
-         * @param value
-         * @return
-         */
-/*
-        private static int rangeCheck(final int value) {
-            while (value > HIGH) {
-                return HIGH;
-            }
-            return value;
-        }
-
-     // factor (increment or decrement)
-        private static int factorZero = 0;
-        private static int factorOne = 1;
-        private static int Low1 = LOW;
-        private static int Low2= LOW;
-        private static int Half = HALF;
-
-        public static Map<Integer, Color> initNumberToColorMap() {
-            HashMap<Integer, Color> Map = new HashMap<Integer, Color>();
-
-            int count = 0;
-            while (true)
-            {
-                //java.awt.Color color;
-                //  color = Map.put(count++, new Color(Low1, Low2, Half));
-                if (Half == HIGH) {
-                    factorZero = 1; // increment
-                }
-                if (Low2 == HIGH) {
-                    factorOne = -1; // decrement
-                    factorZero = +1; // increment
-                }
-                if (Low1 == HIGH || (Low2 == LOW && Half == LOW)) {
-                    factorZero = -1; // decrement
-                }
-
-                if (Low1 < HALF && Half == LOW) {
-                    break; // finish
-                }
-                Low1 += factorZero;
-                Low2 += factorZero;
-                Half += factorOne;
-                Low1 = rangeCheck(Low1);
-                Low2 = rangeCheck(Low2);
-                Half = rangeCheck(Half);
-            }
-            initList(Map);
-            return Map;
-        }
-
-        public static void main(String[] args) {
-            //launch(args);
-        }
+    private static void initList(final HashMap<Integer, Color> localMap) {
+        java.util.List<Integer> list = new ArrayList<Integer>(localMap.keySet());
+        Collections.sort(list);
+        Integer min = list.get(0);
+        Integer max = list.get(list.size() - 1);
+        factor = max + 1;
+    }
 */
-
 }
+
 
 
 
